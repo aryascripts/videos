609d8d40179bed805fc19d9ff37bd2459da4e34a
Aman Bhimani <amanb014@gmail.com>
2020-11-10 20:05:47 -0500

    Nov 11 Video

---
+++ 2020-11-10 mobx/README.md
@@ -0,0 +1,4 @@
+## Mobx Library Introduction
+This video is about creating a MobX Store and to make it work with React JS
+
+## YouTube Link:
---
+++ 2020-11-10 mobx/component.tsx
@@ -0,0 +1,41 @@
+import { observer } from 'mobx-react';
+import React, { useState } from 'react';
+import { TodoStoreImpl } from './TodoStore';
+
+
+interface TodoListProps {
+    todoStore: TodoStoreImpl
+}
+
+export const TodoList: React.FC<TodoListProps> = observer(({todoStore}) => {
+
+    const [value, setValue] = useState<string>('');
+    const status = todoStore.status;
+
+    return <div>
+        <input
+            value={value}
+            onChange={(event) => { 
+                setValue(event.target.value);
+            }}
+            type="text" />  
+        <button onClick={() => {
+            if (value) {
+                todoStore.addTodo(value);
+                setValue('');
+            }
+        }}>submit</button>
+
+        <div>Completed: {status.completed}</div>
+        <div>Remaining: {status.remaining}</div>
+
+        <ul>
+            {todoStore.todos.map(todo => {
+                return <li onClick={() => {
+                    todoStore.toggleTodo(todo.id);
+                }}>[{todo.completed ? 'x' : ' '}] {todo.title}</li>;
+            })}
+        </ul>
+
+    </div>
+});
